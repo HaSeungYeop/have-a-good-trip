60b1952632455e70831c10b06066c9e238353769
haseungyeop <gktmdduq14@naver.com>
2025-07-23 16:39:22 +0900

    백엔드 챗봇 1차

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import chatbot
 
 app = FastAPI()
@@ -14,5 +15,12 @@ async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 app.include_router(chatbot.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 또는 ["http://127.0.0.1:5500"]
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # uvicorn app.main:app --reload
